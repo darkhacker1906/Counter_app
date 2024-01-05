11f561273a5300191c766f1197fa80366b4fe411
darkhacker1906 <singhabhinandan634@gmail.com>
2024-01-05 18:35:08 +0530

    Removed the spacing

--- counter/src/components/SnackBars.jsx
+++ counter/src/components/SnackBars.jsx
@@ -1,28 +1,16 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-import {useEffect} from 'react';
+
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
- function SnackBars({test,setTest,message,rest}) {
-   
+ function SnackBars({test,setTest,message,rest}) { 
   const handleClose = (event, reason) => {
-    // setTimeout(()=>{
-    //   setTest(false);
-    // },1000)
     setTest(false)
   };
-
-// useEffect(() => {
-//   if(test){
-//     handleClose()
-//   }
-// }, [test]);
  
   return (
     <>
